85f0d6cbfae8055f39ae3c4cf9bc73bddd09ceaa
joji-fuck <logvmaks@gmail.com>
2025-04-06 12:26:51 +0500

    add:header

--- resources/views/layout/section/header.blade.php
+++ resources/views/layout/section/header.blade.php
@@ -1,10 +1,14 @@
-<nav>
+<nav class="wrap">
     <a href="{{route('main')}}">
-        <img src="{{asset('images/logo.png')}}">
+        <img src="{{asset('images/logo.png')}}" alt="logo">
     </a>
     <ul>
-        <li>Категории</li>
-        <li>Кнопочки</li>
-        <li>О нас</li>
+        <li><a href="#">Гипотезы</a></li>
+        <li><a href="#">Стартапы</a></li>
+        <li><a href="#">Обеспечение</a></li>
     </ul>
+    <div class="logout">
+        <img src="{{asset('images/user.svg')}}" alt="user">
+        <span>Вход</span>
+    </div>
 </nav>
--- resources/views/register.blade.php
+++ resources/views/register.blade.php
@@ -12,12 +12,12 @@
         <form class="register-form" action="{{route('register')}}" method="post">
             @csrf
             <h1>Регистрация</h1>
-            <input class="name" type="text" name="last_name" placeholder="Фамилия">
-            <input class="name" type="text" name="first_name" placeholder="Имя">
-            <input class="name" type="text" name="patronymic" placeholder="Отчество">
-            <input class="data" type="email" name="email" placeholder="Электронная почта">
+            <input class="name" type="text" name="last_name" placeholder="Фамилия" value="{{old('last_name')}}">
+            <input class="name" type="text" name="first_name" placeholder="Имя" value="{{old('first_name')}}">
+            <input class="name" type="text" name="patronymic" placeholder="Отчество" value="{{old('patronymic')}}">
+            <input class="data" type="email" name="email" placeholder="Электронная почта" value="{{old('email')}}">
             <input class="data" type="password" name="phone" placeholder="Пароль">
-            <input class="data" type="text" name="phone" placeholder="Телефон">
+            <input class="data" type="text" name="phone" placeholder="Телефон" value="{{old('phone')}}">
             <button type="submit">Зарегистрироваться</button>
             <a href="{{route('login.index')}}">У меня есть аккаунт</a>
         </form>
